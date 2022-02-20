cb09fd2461cfd76aad99f1dd59f4a07d6e42878c
Jlee108 <jbacked@yahoo.com>
2022-02-20 15:14:48 +0000

    All tests pass

--- src/main/java/LogLevels.java
+++ src/main/java/LogLevels.java
@@ -1,14 +1,26 @@
 public class LogLevels {
     
     public static String message(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.message() method");
+        //separates the string message into two from the colon
+        String[] res = logLine.split(":");
+        //trim removes all the spaces
+        return res[1].trim();
     }
 
     public static String logLevel(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.logLevel() method");
+        String[] res = logLine.split(":");
+        String logLevel = res[0].toLowerCase();
+        String subString = logLevel.substring(1, logLevel.length()-1);
+
+        return subString;
     }
 
     public static String reformat(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.reformat() method");
+        String[] res = logLine.split(":");
+        String subString = res[0].toLowerCase();
+        String logLevel = res[1].trim() + " " + "(" + subString.substring(1, res[0].length()-1) + ")";
+        String reformat = logLevel.trim();
+
+        return reformat ;
     }
 }
